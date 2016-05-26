18dd4f1f265d67bc427a19ae89a696ca852055ae
smcgee31 <solodesign31@gmail.com>
2016-05-26 14:12:57 -0600

    moving contact page to be an accordion menu

--- public/header/headerDir.js
+++ public/header/headerDir.js
@@ -4,13 +4,25 @@ angular.module('app')
     return {
         templateUrl: 'public/header/header.html',
         restrict: 'AE',
-        // controller: function($scope, mainSvc) {
-        //
-        //     $scope.contactSlide = function() {
-        //
-        //     };
-        //
-        // }
+        controller: function($scope) {
+
+            $scope.visible  = false;
+            $scope.thanks = false;
+
+            $scope.toggle = function() {
+                $scope.visible = !$scope.visible;
+            };
+
+            $scope.thanks = function() {
+                $scope.visible = !$scope.visible;
+                $scope.thanks = !$scope.thanks;
+            };
+
+            $scope.toggle = function() {
+                $scope.class = !$scope.class;
+            };
+
+        }
     };
 
 
