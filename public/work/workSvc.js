08448e9491111040c955e76168025217e5877e6d
smcgee31 <solodesign31@gmail.com>
2016-06-23 11:45:31 -0600

    refactor for hosting

--- public/work/workSvc.js
+++ public/work/workSvc.js
@@ -4,13 +4,13 @@ angular.module('app')
     var workHistory = [
         {
             title: 'Money Waterfall',
-            desc: `This project is intended to be a part of a larger, more robust, website where the company is hoping to entice the potential customer into trying the app and more fully understand one of the many financial things that they could do for the customer. This application takes the users debts in, and when the user decides how much more they can afford to apply to thier debts, will project a "debt freedom" date using a "waterfall" effect on the debts.`,
+            desc: `This project is intended to be a piece of a larger website where the company is hoping to entice the potential customer into trying the simulation app and more fully understand one of the many financial things that they could do for the customer. This application takes the users debts in, and when the user decides how much more they can afford to apply to thier debts, will project a "debt freedom" date using a "waterfall" effect on the debts.`,
             tech: ['HTML', 'CSS', 'AngularJS', 'Node.js', 'Express', 'MongoDB'],
             img: 'public/img/mwaterfall.png',
             url: `http://moneywaterfall.stevenmcgee.me`
         },{
-            title: 'iTunes Searcher',
-            desc: 'This was a wonderful project that I completely stumbled through while I was a student at DevMountain. Good learning though.',
+            title: 'DevTunes',
+            desc: 'This was an AngularJS class project that I completed while at DevMountain. It will search iTunes for your artist and return a grid with a list of songs with all the other iTunes information about those songs. PS. it looks fine down to an iPad but a cell phone... not so much - needs work there.',
             tech: ['HTML', 'CSS', 'AngularJS'],
             img: 'public/img/iTunesSearcher.png',
             url: `http://itunesseacher.stevenmcgee.me`
--- server/index.js
+++ server/index.js
@@ -1,21 +1,10 @@
-// Require dependencies
-var express = require('express');
-var bodyParser = require('body-parser');
-
-
-// initilize app
+// Portfolio
+var express = require( 'express' );
 var app = express();
-
-// initilize dependencies
-app.use(bodyParser.json());
-
-
-// Variables
 var port = 4000;
-var mongoURI = 'mongodb://localhost:27017/DBName';
 
-// MongoDB connection
-// App Listen
-app.listen(port, function() {
-    console.log('listening on port ', port);
-});
+app.use(express.static(__dirname));
+
+app.listen( port, function () {
+	console.log( 'listening on port ', port );
+} );
