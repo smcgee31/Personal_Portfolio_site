acfe8422de3e44b43d7d5000b14004892a888212
smcgee31 <solodesign31@gmail.com>
2016-06-24 11:33:05 -0600

    again minor fixes for hosting

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ var express = require( 'express' );
 var app = express();
 var port = 4000;
 
-app.use(express.static(__dirname+'/../public'));
+app.use( express.static( __dirname + '/../public' ) );
 
 app.listen( port, function () {
 	console.log( 'listening on port ', port );
